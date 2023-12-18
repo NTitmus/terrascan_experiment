1e411957cddf85fb3dbff0389c8deb36efbb8233
Nicola Titmus <nicola.koo@cantab.net>
2023-12-18 13:53:16 +0000

    added other infrastructure

--- main.tf
+++ main.tf
@@ -21,6 +21,11 @@ terraform {
   }
 }
 
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
 resource "aws_vpc" "new_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -29,7 +34,7 @@ resource "aws_vpc" "new_vpc" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "a_subnet" {
   vpc_id     = aws_vpc.new_vpc.id
   cidr_block = "10.0.4.0/24"
   availability_zone = "1a"
@@ -39,10 +44,62 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "some_ig" {
+  vpc_id = aws_vpc.new_vpc.id
+
+  tags = {
+    Name = "Some Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.new_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.some_ig.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.a_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "HTTP and SSH"
+  vpc_id = aws_vpc.new_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   instance_type = "t2.micro"
   ami           = "ami-0cfd0973db26b893b"
-  subnet_id     = aws_subnet.private_subnet.id
+  subnet_id     = aws_subnet.a_subnet.id
 
   metadata_options {
     http_endpoint = "enabled"
